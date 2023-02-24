17e2e1f2458828fb1673e36edfb0e2965a3be98f
Otavio Vasconcelos <otaviocavasc2@gmail.com>
2023-02-24 14:39:27 -0300

    Restaurant model implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "restaurants#index"
+
+  resources :restaurants, only: %i[index new create show]
 end
